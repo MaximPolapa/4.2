1b948fdc8be8e3a0b2481d9e04ae5ac3d21a41fc
MaximPolapa <n0220po12@gmail.com>
2021-10-07 21:02:13 +0300

    Lab4.2

---
+++ 4.2/4.2/Source.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+	double x, xp, xk, dx, A, B, y;
+
+	cout << "xp = "; cin >> xp;
+	cout << "xk = "; cin >> xk;
+	cout << "dx = "; cin >> dx;
+	cout << fixed;
+	cout << "---------------------------" << endl;
+	cout << "|" << setw(7) << "x" << " |" << setw(10) << "y" << " |" << endl;
+	cout << "---------------------------" << endl;
+
+	x = xp;
+	while (x <= xk)
+	{
+		A = 13.5 - 2 * x;
+
+		if (x <= -1)
+			B = exp(0.4 + x);
+		else
+			if (-1 < x && 1 > x)
+				B = 1 - sin(x) * sin(x);
+			else
+				B = cos(x) / (1 + sin(x) * sin(x));
+		y = A - B;
+		cout << "|" << setw(7) << setprecision(2) << x
+			<< " |" << setw(10) << setprecision(3) << y
+			<< " |" << endl;
+		x += dx;
+	}
+	cout << "---------------------------" << endl;
+	return 0;
+}
